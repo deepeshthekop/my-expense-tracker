d2bd0816bc53b3180cd0ebd441d3e4465bd1526d
Shreyan Dey <deyshreyan24@outlook.com>
2024-08-19 13:29:43 +0530

    Connect database

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -1,10 +1,36 @@
-import { Text } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Box, Heading, Text, Table } from "@radix-ui/themes";
+import exp from "constants";
 
-function ExpensesPage() {
+async function ExpensesPage() {
+  const expenses = await prisma.expense.findMany();
+
   return (
-    <div>
-      <Text className="text-3xl">Expenses</Text>
-    </div>
+    <Box className="m-10">
+      <Heading size="8" className="mt-10">
+        Expenses
+      </Heading>
+      <Table.Root variant="surface" className="mt-5">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Amount</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Date</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Category</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {expenses.map((expense) => (
+            <Table.Row key={expense.id}>
+              <Table.Cell>$ {expense.amount}</Table.Cell>
+              <Table.Cell>{expense.date.toDateString()}</Table.Cell>
+              <Table.Cell>{expense.title}</Table.Cell>
+              <Table.Cell>{expense.category}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Box>
   );
 }
 
